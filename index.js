25009b51acccd72ef88f996313e8a098e763cf2b
Malcolm <sciand6@gmail.com>
2021-03-06 00:32:25 -0500

    Initial commit.

---
+++ index.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const app = express();
+require("dotenv").config();
+const articleRouter = require("./routes/articles");
+const mongoose = require("mongoose");
+const Article = require("./models/article");
+const methodOverride = require("method-override");
+
+mongoose.connect(process.env.mongoURI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useCreateIndex: true,
+});
+
+app.set("view engine", "ejs");
+app.use(express.urlencoded({ extended: false }));
+app.use(methodOverride("_method"));
+
+app.use("/articles", articleRouter);
+
+app.get("/", async (req, res) => {
+  const articles = await Article.find().sort({ createdAt: "desc" });
+  res.render("articles/index", { articles: articles });
+});
+
+app.listen(5000);
